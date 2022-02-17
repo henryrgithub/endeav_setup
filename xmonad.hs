be41fa4329b760972122a6f07ac820153c8f2dce
henryrgithub <hroeramsey@gmail.com>
2022-02-16 21:31:10 -0800

    now have xmobar installing and setup

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,8 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 
-main = xmonad defaultConfig
+main = xmonad $ docks def
 	{ terminal = "xterm"
+	, layoutHook=avoidStruts $ layoutHook def
+	, manageHook=manageHook def <+> manageDocks
 	} 
